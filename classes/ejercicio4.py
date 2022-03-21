7f9ea984aa3bd0af7a6c25b3af7d3fc10c951326
Germiprogramer <llorentemunozgerman@gmail.com>
2022-03-21 18:24:41 +0100

    correccion ejercicio 2

---
+++ classes/ejercicio4.py
@@ -0,0 +1,20 @@
+class Logger:
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+    contador = 1
+
+    def log(self, contador):
+        contador +=1
+
+class Test():
+    def __init__(self, contador):
+        self.contador = contador
+    
+    def llamada(string):
+        
+
+
+
+    
+
+        
